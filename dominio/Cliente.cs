e7963079ed31a95457d7ccccc6da34443173b6cd
CataAvi <avilarotelcatlina@gmail.com>
2024-05-30 22:30:07 -0300

    Se crea proyecto con capa de dominio y clases

---
+++ dominio/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dominio
+{
+    public class Cliente
+    {
+        int codigo  { get; set; }
+        string nombre  { get; set; }
+        string telefono  { get; set; }
+        string mail  { get; set; }
+        
+    
+}
